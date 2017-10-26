03a5830f9386226fc508c05133082feec7a42dba
unknown <luca.canizzo@gmail.com>
2017-10-26 19:22:54 -0400

    Projects Rendering with Handlebars

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,6 @@
 const projects = [
     {
-        initials : "N P",
+        initials : "N.P",
         name : "National Parks Trivia",
         technologies : ["jQuery", "Javascript", "Bootstrap", "AJAX"],
         description : "A tallied and timed trivia game featuring facts about the United State's National Parks.",
@@ -8,13 +8,37 @@ const projects = [
         repoLink : "https://github.com/lcanizzo/National-Parks-Trivia"
     },
     {
-        initials : "N P",
-        name : "National Parks Trivia",
-        technologies : ["jQuery", "Javascript", "Bootstrap", "AJAX"],
-        description : "A tallied and timed trivia game featuring facts about the United State's National Parks.",
-        appLink : "https://lcanizzo.github.io/National-Parks-Trivia/",
-        repoLink : "https://github.com/lcanizzo/National-Parks-Trivia"
-    }
+        initials : "Q.G",
+        name : "Quick Gifs",
+        technologies : ["jQuery", "Javascript", "Bootstrap", "SPA", "APIs"],
+        description : "A responsive single page app to search through Gifs with various filters.",
+        appLink : "https://lcanizzo.github.io/Quick-Gifs/",
+        repoLink : "https://github.com/lcanizzo/Quick-Gifs"
+    },
+    {
+        initials : "G.S",
+        name : "Game Swap",
+        technologies : ["SQL", "Express", "node", "Handlebars", "Javascript", "APIs", "E-mailer", "Passport-Auth"],
+        description : "A unique web app connecting University students who would like to exchange video games with peers in the GameSwap network.",
+        appLink : "https://sleepy-oasis-78182.herokuapp.com/",
+        repoLink : "https://github.com/lcanizzo/game_swap"
+    },
+    {
+        initials : "S.G",
+        name : "School Guide",
+        technologies : ["d3", "jQuery", "Javascript", "Bootstrap", "SPA", "APIs", "CSS"],
+        description : "A dynamic website to search through Colleges and Universities with clear tables of key criteria and intuitive user flow to pages with detailed results.",
+        appLink : "https://lcanizzo.github.io/School_guide/",
+        repoLink : "https://github.com/lcanizzo/School_guide"
+    },
+    {
+        initials : "S.B",
+        name : "Study Buddy",
+        technologies : ["jQuery", "Javascript", "Bootstrap", "SQL", "AJAX", "Express", "node", "CSS"],
+        description : "(In Development) A mobile-first app that matches students to trade tutoring services.",
+        appLink : "https://shrouded-basin-79387.herokuapp.com/",
+        repoLink : "https://github.com/lcanizzo/Study_Buddy"
+    },
 ];
 
 module.exports= projects;
