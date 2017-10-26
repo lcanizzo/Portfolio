4706bb094eba1073406c386dc594d2e225c6ef53
unknown <luca.canizzo@gmail.com>
2017-10-26 18:37:40 -0400

    Adjusted Routes

--- controller/controller.js
+++ controller/controller.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (request, response)=>{
-    response.render("index", {layout: 'main'});
+    response.render("index");
     console.log("Home Rendered");
+})
+
+router.get("/work", (req, res)=>{
+    res.render("work");
+    console.log("Work Renderd");
+})
+
+router.get("/contact", (req,res)=>{
+    res.render("contact");
+    console.log("Contact Rendered");
+})
+
+router.get("/bio", (req,res)=>{
+    res.render("bio");
+    console.log("Bio Renderd");
 })
 
 module.exports = router
