542672d292d13c180ae46c5e914d4e64a4550883
unknown <luca.canizzo@gmail.com>
2017-10-26 22:15:41 -0400

    Added status to projects

--- projects/projects.js
+++ projects/projects.js
@@ -6,7 +6,8 @@ const projects = [
         description : "A unique web app connecting University students who would like to exchange video games with peers in the GameSwap network.",
         appLink : "https://sleepy-oasis-78182.herokuapp.com/",
         repoLink : "https://github.com/lcanizzo/game_swap",
-        color: "#c7daff"
+        color: "#c7daff",
+        status: 0
     },
     {
         initials : "N.P",
@@ -15,7 +16,8 @@ const projects = [
         description : "A tallied and timed trivia game featuring facts about the United State's National Parks.",
         appLink : "https://lcanizzo.github.io/National-Parks-Trivia/",
         repoLink : "https://github.com/lcanizzo/National-Parks-Trivia",
-        color : "#acfdb0"
+        color : "#acfdb0",
+        status: 1
     },
     {
         initials : "S.G",
@@ -24,7 +26,8 @@ const projects = [
         description : "A dynamic website to search through Colleges and Universities with clear tables of key criteria and intuitive user flow to pages with detailed results.",
         appLink : "https://lcanizzo.github.io/School_guide/",
         repoLink : "https://github.com/lcanizzo/School_guide",
-        color: "#ffc7c7"
+        color: "#ffc7c7",
+        status: 0        
     },
     {
         initials : "Q.G",
@@ -33,7 +36,8 @@ const projects = [
         description : "A responsive single page app to search through Gifs with various filters.",
         appLink : "https://lcanizzo.github.io/Quick-Gifs/",
         repoLink : "https://github.com/lcanizzo/Quick-Gifs",
-        color: "#ffc7fe"
+        color: "#ffc7fe",
+        status: 1        
     },
     {
         initials : "S.B",
@@ -42,7 +46,8 @@ const projects = [
         description : "(In Development) A mobile-first app that matches students to trade tutoring services.",
         appLink : "https://shrouded-basin-79387.herokuapp.com/",
         repoLink : "https://github.com/lcanizzo/Study_Buddy",
-        color: "#fdd0ac"
+        color: "#fdd0ac",
+        status: 0        
     },
 ];
 
